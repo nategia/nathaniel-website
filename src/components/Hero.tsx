a427958ccc5e9ecae273d66b6edd547525be44ee
Nate <nate@calculated.fi>
2023-09-21 12:42:06 +1000

    button styles hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,6 +4,16 @@ function Hero() {
   const [isLargerThan800] = useMediaQuery("(min-width: 800px)");
   const [isLargerThan500] = useMediaQuery("(min-width: 500px)");
 
+  const buttonStyles = {
+    size: isLargerThan500 ? "md" : "sm",
+    height: isLargerThan500 ? "48px" : "32px",
+    width: isLargerThan500 ? "200px" : "auto",
+    border: "1px",
+    variant: "outline",
+    borderColor: "whiteAlpha.800",
+    color: "whiteAlpha.800",
+  };
+
   return (
     <Stack
       h="xl"
@@ -38,24 +48,8 @@ function Hero() {
             With a strong focus on user-experience and clean code.
           </Heading>
           <Stack spacing={4} direction={isLargerThan500 ? "row" : "column"}>
-            <Button
-              size={isLargerThan500 ? "md" : "sm"}
-              height={isLargerThan500 ? "48px" : "32px"}
-              width={isLargerThan500 ? "200px" : "auto"}
-              border="2px"
-              variant="outline"
-            >
-              Hire
-            </Button>
-            <Button
-              size={isLargerThan500 ? "md" : "sm"}
-              height={isLargerThan500 ? "48px" : "32px"}
-              width={isLargerThan500 ? "200px" : "auto"}
-              border="2px"
-              variant="outline"
-            >
-              Contact
-            </Button>
+            <Button {...buttonStyles}>Hire</Button>
+            <Button {...buttonStyles}>Contact</Button>
           </Stack>
         </Stack>
       </Flex>
