64dbc7033c8b9fac6f4ed616d44112cfe13729f3
Nate <nate@calculated.fi>
2023-09-22 22:31:02 +1000

    deploy error

--- src/pages/features/index.page.tsx
+++ src/pages/features/index.page.tsx
@@ -1,33 +1,24 @@
-import { NextPage } from "next"
-import { Box, Text } from '@chakra-ui/react';
-import Header from "../../components/Header"
-import Footer from "../../components/Footer"
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-
-
+import { NextPage } from "next";
+import Footer from "../../components/Footer";
+// import styles from '../styles/Home.module.css'
 
 const Features: NextPage = () => {
   return (
-    <Box className={styles.container}>
-      <Header />
-      <Head>
-        <title>Nathaniel Codes</title>
-        <meta name="description" content="Front-end developer" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    // <Box className={styles.container}>
+    //   <Header />
+    //   <Head>
+    //     <title>Nathaniel Codes</title>
+    //     <meta name="description" content="Front-end developer" />
+    //     <link rel="icon" href="/favicon.ico" />
+    //   </Head>
 
-      <Box py={2} minHeight="calc(100vh - 64px)"   >
-        <Text>Features</Text>
-      </Box>
+    //   <Box py={2} minHeight="calc(100vh - 64px)"   >
+    //     <Text>Features</Text>
+    //   </Box>
 
+    <Footer />
+    // </Box>
+  );
+};
 
-
-
-      <Footer />
-    </Box>
-  )
-}
-
-export default Features
+export default Features;
